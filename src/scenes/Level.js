332f7ffed387f19e451b1c8579a655f6f3b8b222
yudiz-fenil <fenil.babariya@yudiz.com>
2023-07-11 18:14:01 +0530

    gameover and winning added

--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -16,41 +16,31 @@ class Level extends Phaser.Scene {
 	/** @returns {void} */
 	editorCreate() {
 
-		// title
-		const title = this.add.text(540, 182, "", {});
-		title.setOrigin(0.5, 0.5);
-		title.text = "2048 Balls";
-		title.setStyle({ "align": "center", "fontFamily": "Verdana", "fontSize": "100px" });
+		// container_body
+		const container_body = this.add.container(0, 0);
 
-		// btn_replay
-		const btn_replay = this.add.image(148, 189, "replay");
-		btn_replay.scaleX = 0.15;
-		btn_replay.scaleY = 0.15;
-		btn_replay.tintFill = true;
-		btn_replay.tintTopLeft = 3157807;
-		btn_replay.tintTopRight = 3157807;
-		btn_replay.tintBottomLeft = 3157807;
-		btn_replay.tintBottomRight = 3157807;
-
-		// end_line
-		const end_line = this.add.rectangle(540, 489, 1080, 10);
-		end_line.visible = false;
-		end_line.isFilled = true;
+		// game_title
+		const game_title = this.add.text(540, 182, "", {});
+		game_title.setOrigin(0.5, 0.5);
+		game_title.text = "2048 Balls";
+		game_title.setStyle({ "align": "center", "fontFamily": "Verdana", "fontSize": "100px" });
+		container_body.add(game_title);
 
 		// box
 		const box = this.add.image(540, 1118, "box");
 		box.scaleX = 1.45;
 		box.scaleY = 1.3;
+		container_body.add(box);
 
-		// dashed_line
-		const dashed_line = this.add.image(540, 490, "dashed-line");
-		dashed_line.scaleX = 1.35;
+		// container_balls
+		const container_balls = this.add.container(0, 0);
 
 		// container_ball_tracker
-		const container_ball_tracker = this.add.container(840, 416);
+		const container_ball_tracker = this.add.container(840, 466);
 
 		// ball_tracker
-		const ball_tracker = this.add.ellipse(0, 0, 100, 100);
+		const ball_tracker = this.add.ellipse(0, 0, 200, 200);
+		ball_tracker.alpha = 0.5;
 		ball_tracker.isFilled = true;
 		ball_tracker.fillColor = 7479055;
 		ball_tracker.isStroked = true;
@@ -62,34 +52,71 @@ class Level extends Phaser.Scene {
 		const txt_ball = this.add.text(0, 0, "", {});
 		txt_ball.setOrigin(0.5, 0.5);
 		txt_ball.text = "2";
-		txt_ball.setStyle({ "align": "center", "color": "#cc381a", "fontFamily": "Verdana", "fontSize": "60px", "fontStyle": "bold" });
+		txt_ball.setStyle({ "align": "center", "color": "#cc381a", "fontFamily": "Verdana", "fontSize": "80px", "fontStyle": "bold" });
 		container_ball_tracker.add(txt_ball);
 
 		// ball_tracker_border
-		const ball_tracker_border = this.add.ellipse(0, 0, 100, 100);
+		const ball_tracker_border = this.add.ellipse(0, 0, 200, 200);
 		ball_tracker_border.fillColor = 13383706;
 		ball_tracker_border.isStroked = true;
 		ball_tracker_border.strokeColor = 13383706;
 		ball_tracker_border.lineWidth = 5;
 		container_ball_tracker.add(ball_tracker_border);
 
-		// title (components)
-		new PreloadText(title);
+		// container_endline
+		const container_endline = this.add.container(0, 0);
 
-		this.btn_replay = btn_replay;
-		this.end_line = end_line;
+		// end_line
+		const end_line = this.add.rectangle(540, 627, 1080, 10);
+		end_line.visible = false;
+		end_line.isFilled = true;
+		container_endline.add(end_line);
+
+		// dashed_line
+		const dashed_line = this.add.image(540, 628, "dashed-line");
+		dashed_line.scaleX = 1.35;
+		container_endline.add(dashed_line);
+
+		// container_gameover
+		const container_gameover = this.add.container(0, 0);
+
+		// bg_rect
+		const bg_rect = this.add.rectangle(540, 960, 1080, 1920);
+		bg_rect.visible = false;
+		bg_rect.isFilled = true;
+		bg_rect.fillColor = 0;
+		bg_rect.fillAlpha = 0.5;
+		container_gameover.add(bg_rect);
+
+		// btn_replay
+		const btn_replay = this.add.image(150, 190, "replay");
+		btn_replay.scaleX = 0.15;
+		btn_replay.scaleY = 0.15;
+		btn_replay.tintFill = true;
+		btn_replay.tintTopLeft = 16777215;
+		btn_replay.tintTopRight = 16777215;
+		btn_replay.tintBottomLeft = 16777215;
+		btn_replay.tintBottomRight = 16777215;
+		container_gameover.add(btn_replay);
+
+		// game_title (components)
+		new PreloadText(game_title);
+
+		this.container_balls = container_balls;
 		this.container_ball_tracker = container_ball_tracker;
 		this.ball_tracker = ball_tracker;
 		this.txt_ball = txt_ball;
 		this.ball_tracker_border = ball_tracker_border;
+		this.end_line = end_line;
+		this.container_gameover = container_gameover;
+		this.bg_rect = bg_rect;
+		this.btn_replay = btn_replay;
 
 		this.events.emit("scene-awake");
 	}
 
-	/** @type {Phaser.GameObjects.Image} */
-	btn_replay;
-	/** @type {Phaser.GameObjects.Rectangle} */
-	end_line;
+	/** @type {Phaser.GameObjects.Container} */
+	container_balls;
 	/** @type {Phaser.GameObjects.Container} */
 	container_ball_tracker;
 	/** @type {Phaser.GameObjects.Ellipse} */
@@ -98,6 +125,14 @@ class Level extends Phaser.Scene {
 	txt_ball;
 	/** @type {Phaser.GameObjects.Ellipse} */
 	ball_tracker_border;
+	/** @type {Phaser.GameObjects.Rectangle} */
+	end_line;
+	/** @type {Phaser.GameObjects.Container} */
+	container_gameover;
+	/** @type {Phaser.GameObjects.Rectangle} */
+	bg_rect;
+	/** @type {Phaser.GameObjects.Image} */
+	btn_replay;
 
 	/* START-USER-CODE */
 
@@ -106,21 +141,20 @@ class Level extends Phaser.Scene {
 	create() {
 		this.oGameManager = new GameManager(this);
 		this.editorCreate();
-
 		this.ballsGroup = this.add.group();
 		this.countMoves = 0;
+		this.isGameOver = false;
 		this.oBalls = this.oGameManager.oBalls;
 		this.btn_replay.setInteractive().on('pointerdown', () => this.scene.restart());
 		this.physics.add.existing(this.end_line);
 		this.physics.add.collider(this.ballsGroup, this.ballsGroup, this.mergeballs);
-		this.physics.add.collider(this.ballsGroup, this.end_line, this.gameOver);
+		this.physics.add.overlap(this.ballsGroup, this.end_line, () => this.gameOver());
 		this.input.on('pointermove', this.trackBall);
-		this.input.on('pointerdown', this.getBall);
+		this.input.on('pointerdown', () => this.generateBall(this.txt_ball.text));
 	}
 
 	mergeballs = (ball1, ball2) => {
 		if (ball1.name == ball2.name) {
-
 			const newSize = parseInt(ball1.name) * 2;
 
 			ball1.setName(newSize);
@@ -137,56 +171,74 @@ class Level extends Phaser.Scene {
 			const body = ball1.body;
 			body.setSize(this.oBalls[newSize].nSize, this.oBalls[newSize].nSize);
 			body.setCircle(this.oBalls[newSize].nSize / 2);
-			body.setBounce(0.8);
+			body.setBounce(0.7);
 
 			ball1.list[1].setOrigin(0.5, 0.5);
 			ball1.list[1].setPosition(ball1.list[0].x, ball1.list[0].y);
 
+			if (newSize == 2048) this.winGame();
 			ball2.destroy();
+		} else {
+			ball1.body.setFriction(0);
+			ball2.body.setFriction(0);
+			ball1.body.setBounce(0.8);
+			ball2.body.setBounce(0.8);
 		}
 	}
 
 	generateBall = (sBall) => {
-		const container = this.add.container(this.container_ball_tracker.x, 560);
-		const ball = this.add.ellipse(0, 0, this.oBalls[sBall].nSize, this.oBalls[sBall].nSize, this.oBalls[sBall].nColor, 0.5);
-		const border = this.add.ellipse(0, 0, this.oBalls[sBall].nSize, this.oBalls[sBall].nSize);
-		border.isStroked = true;
-		border.strokeColor = this.oBalls[sBall].nBorder;
-		border.lineWidth = 3;
-		const text = this.add.text(0, 0, this.oBalls[sBall].nLabel, {
-			fontSize: this.oBalls[sBall].sFontSize,
-			color: this.oBalls[sBall].sColor,
-			align: "center",
-			fontStyle: "bold",
-			fontFamily: "Verdana",
-		});
-		text.setOrigin(0.5, 0.5);
-		container.add([ball, text, border]);
-		this.physics.world.enable(container);
-		container.setName(this.oBalls[sBall].nLabel);
-		this.ballsGroup.add(container);
-		const containerBody = container.body;
-		containerBody.setCollideWorldBounds(true);
-		containerBody.setCircle(ball.width / 2);
-		containerBody.setBounce(0.3);
-		containerBody.setOffset(-ball.width / 2, -ball.height / 2);
-		containerBody.gravity.y = 2000;
-		this.handleTracker();
+		if (this.container_ball_tracker.visible && !this.isGameOver) {
+			const container = this.add.container(this.container_ball_tracker.x, this.end_line.y + 180);
+			const ball = this.add.ellipse(0, 0, this.oBalls[sBall].nSize, this.oBalls[sBall].nSize, this.oBalls[sBall].nColor, 0.5);
+			const border = this.add.ellipse(0, 0, this.oBalls[sBall].nSize, this.oBalls[sBall].nSize);
+			border.isStroked = true;
+			border.strokeColor = this.oBalls[sBall].nBorder;
+			border.lineWidth = 3;
+			const text = this.add.text(0, 0, this.oBalls[sBall].nLabel, {
+				fontSize: this.oBalls[sBall].sFontSize,
+				color: this.oBalls[sBall].sColor,
+				align: "center",
+				fontStyle: "bold",
+				fontFamily: "Verdana",
+			});
+			text.setOrigin(0.5, 0.5);
+			container.add([ball, text, border]);
+			this.container_balls.add(container);
+			this.physics.world.enable(container);
+			container.setName(this.oBalls[sBall].nLabel);
+			this.ballsGroup.add(container);
+			const containerBody = container.body;
+			containerBody.setCollideWorldBounds(true);
+			containerBody.setCircle(ball.width / 2);
+			containerBody.setBounce(0.3);
+			containerBody.setMass(10);
+			containerBody.setOffset(-ball.width / 2, -ball.height / 2);
+			containerBody.gravity.y = 4000;
+			this.handleTracker();
+		}
 	}
-
 	trackBall = (pointer) => {
 		let x = pointer.x;
-		x = Math.min(Math.max(100, x), 980);
+		x = Math.min(Math.max(145, x), 935);
 		this.container_ball_tracker.x = x;
 	}
-
 	getBall = () => {
-		if (this.container_ball_tracker.visible) {
-			const balls = [2, 2, 2, 4, 4, 4, 8, 8, 16]
-			const n = Math.floor(Math.random() * balls.length);
-			this.countMoves++;
-			this.generateBall(this.countMoves > 5 ? balls[n] : 2);
-		}
+		const balls = [2, 4, 8, 2, 4, 8, 2, 4, 8, 16, 16]
+		// const balls = [16]
+		const n = Math.floor(Math.random() * balls.length);
+		this.countMoves++;
+		this.setBall(this.countMoves > 4 ? balls[n] : 2);
+	}
+	setBall = (nBall) => {
+		this.ball_tracker.setFillStyle(this.oBalls[nBall].nColor);
+		this.ball_tracker.setSize(this.oBalls[nBall].nSize, this.oBalls[nBall].nSize);
+
+		this.ball_tracker_border.strokeColor = this.oBalls[nBall].nBorder;
+		this.ball_tracker_border.setSize(this.oBalls[nBall].nSize, this.oBalls[nBall].nSize);
+
+		this.txt_ball.setText(this.oBalls[nBall].nLabel);
+		this.txt_ball.setColor(this.oBalls[nBall].sColor);
+		this.txt_ball.setFontSize(this.oBalls[nBall].sFontSize);
 	}
 	handleTracker = () => {
 		if (this.container_ball_tracker.visible) {
@@ -196,10 +248,36 @@ class Level extends Phaser.Scene {
 		}
 		setTimeout(() => {
 			this.container_ball_tracker.setVisible(true);
+			this.getBall();
 		}, 1000);
 	}
 	gameOver = () => {
-		this.scene.restart();
+		if (!this.isGameOver) this.btnRestartAnimation();
+	}
+	winGame = () => {
+		this.btnRestartAnimation();
+		this.showConfetti();
+	}
+	btnRestartAnimation = () => {
+		this.isGameOver = true;
+		this.bg_rect.setVisible(true);
+		this.container_ball_tracker.setVisible(false);
+		this.tweens.add({
+			targets: this.btn_replay,
+			x: 540,
+			y: 960,
+			scaleX: 1,
+			scaleY: 1,
+			ease: "Bounce",
+			duration: 500,
+		})
+	}
+	showConfetti = () => {
+		confetti({
+			particleCount: 80,
+			spread: 40,
+			origin: { y: 0.5 },
+		});
 	}
 
 	/* END-USER-CODE */
--- src/scripts/GameManager.js
+++ src/scripts/GameManager.js
@@ -2,37 +2,37 @@ class GameManager {
 	constructor(oScene) {
 		this.oBalls = {
 			2: {
-				nLabel: 2, nSize: 100, nColor: 0x721f0f, sColor: "#cc381a", nBorder: 0xcc381a, sFontSize: "60px"
+				nLabel: 2, nSize: 200, nColor: 0x721f0f, sColor: "#cc381a", nBorder: 0xcc381a, sFontSize: "80px"
 			},
 			4: {
-				nLabel: 4, nSize: 110, nColor: 0x7b7900, sColor: "#bcb801", nBorder: 0xbcb801, sFontSize: "70px"
+				nLabel: 4, nSize: 220, nColor: 0x7b7900, sColor: "#bcb801", nBorder: 0xbcb801, sFontSize: "90px"
 			},
 			8: {
-				nLabel: 8, nSize: 120, nColor: 0x487609, sColor: "#72ba0c", nBorder: 0x72ba0c, sFontSize: "80px"
+				nLabel: 8, nSize: 240, nColor: 0x487609, sColor: "#72ba0c", nBorder: 0x72ba0c, sFontSize: "100px"
 			},
 			16: {
-				nLabel: 16, nSize: 130, nColor: 0x267479, sColor: "#44c4cc", nBorder: 0x44c4cc, sFontSize: "80px"
+				nLabel: 16, nSize: 260, nColor: 0x267479, sColor: "#44c4cc", nBorder: 0x44c4cc, sFontSize: "100px"
 			},
 			32: {
-				nLabel: 16, nSize: 140, nColor: 0x493978, sColor: "#7c61cc", nBorder: 0x7c61cc, sFontSize: "80px"
+				nLabel: 32, nSize: 280, nColor: 0x493978, sColor: "#7c61cc", nBorder: 0x7c61cc, sFontSize: "100px"
 			},
 			64: {
-				nLabel: 16, nSize: 150, nColor: 0x113577, sColor: "#235bcb", nBorder: 0xc235bcb, sFontSize: "80px"
+				nLabel: 64, nSize: 300, nColor: 0x113577, sColor: "#235bcb", nBorder: 0xc235bcb, sFontSize: "100px"
 			},
 			128: {
-				nLabel: 16, nSize: 160, nColor: 0x7e3060, sColor: "#c74b98", nBorder: 0xc74b98, sFontSize: "70px"
+				nLabel: 128, nSize: 320, nColor: 0x7e3060, sColor: "#c74b98", nBorder: 0xc74b98, sFontSize: "90px"
 			},
 			256: {
-				nLabel: 16, nSize: 170, nColor: 0x487609, sColor: "#72ba0c", nBorder: 0x72ba0c, sFontSize: "70px"
+				nLabel: 256, nSize: 330, nColor: 0x487609, sColor: "#72ba0c", nBorder: 0x72ba0c, sFontSize: "90px"
 			},
 			512: {
-				nLabel: 16, nSize: 180, nColor: 0x825400, sColor: "#cc8301", nBorder: 0xcc8301, sFontSize: "70px"
+				nLabel: 512, nSize: 340, nColor: 0x825400, sColor: "#cc8301", nBorder: 0xcc8301, sFontSize: "90px"
 			},
 			1024: {
-				nLabel: 16, nSize: 190, nColor: 0x337354, sColor: "#56c18c", nBorder: 0x56c18c, sFontSize: "60px"
+				nLabel: 1024, nSize: 360, nColor: 0x337354, sColor: "#56c18c", nBorder: 0x56c18c, sFontSize: "90px"
 			},
 			2048: {
-				nLabel: 16, nSize: 200, nColor: 0x504f50, sColor: "#ffffff", nBorder: 0x504f50, sFontSize: "60px"
+				nLabel: 2048, nSize: 380, nColor: 0x504f50, sColor: "#ffffff", nBorder: 0x504f50, sFontSize: "90px"
 			},
 		}
 	}
